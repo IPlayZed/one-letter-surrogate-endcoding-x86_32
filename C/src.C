69f7769b035d06e11f717a4efae61651d57ca72a
IPlayZed <pauljouser@gmail.com>
2020-04-18 19:06:20 +0200

    Goto prototype

--- C/src.C
+++ C/src.C
@@ -4,19 +4,26 @@ void encrypt(char *input, char key[], char *output)
 {
     int i=0;
     char abc[]="abcdefghijklmnopqrstuvwxyz";
+
+    //loop********************************************
     while (input[i]!='\0')
     {
         //printf("bemenet[%d]:%c\n\n", i, input[i]);
+
+        //loop_inner**********************************
         int j=0;
         while (input[i]!=abc[j])
         {
             //printf("abd[%d]:%c\n", j, abc[j]);
             j++;
         }
+        //loop_inner**********************************
+        
         output[i]=key[j];
         //printf("key[%d]:%c\n", j, key[j]);
         i++;    
     }
+    //loop********************************************
     output[i]='\0';
 }
 
