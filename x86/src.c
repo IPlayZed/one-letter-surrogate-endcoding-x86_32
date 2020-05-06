23ea93ead447beb8f97cd9276266074da38d4078
IPlayZed <pauljouser@gmail.com>
2020-05-06 21:59:33 +0200

    Project kész Commentek a könnyű értelmezéshez Szemét feltisztítva

---
+++ x86/src.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void extern encrypt (char *input, char key [26], char * output);
+
+int main(){
+
+    char *input="abcz";
+    char key[27]="DKVQFIBJWPESCXHTMYAUOLRGZN";
+    char output[20];
+
+    encrypt (input, key, output);
+
+    printf("%s -> %s \n" ,input ,output );
+    return 0;
+}
